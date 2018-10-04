577a3e6849d84c7c7c0d5b67ce6ae29559231266
Daniel <daniel.landberg@gmail.com>
2018-10-03 18:12:50 -0700

    Update mirror_swap.sh

--- mirror_swap.sh
+++ mirror_swap.sh
@@ -5,24 +5,14 @@
 # <bitbar.author>ThisCodedBox</bitbar.author>
 # <bitbar.author.github>ThisCodedBox</bitbar.author.github>
 # <bitbar.desc>Swap between mirror mode and a rotated display to effectively disable a screen.</bitbar.desc>
-
+# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/thiscodedbox/mirror_swap/</bitbar.abouturl>
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 mirror_stat=$($parent_path/mirror_swap_exec/mirror -q)
 
 echo "🖥"
 echo "---"
-# echo "Mirror (Disable) | bash='/Users/walrusx4/Documents/Source/fb-rotate/fb-rotate -d 0x1cab4f8c -r 0 && /Users/walrusx4/mirror-screen/mirror -l QHD270 1' param1= terminal=false"
-
-# echo "Disable (Restore) | bash='/Users/walrusx4/mirror-screen/mirror && /Users/walrusx4/Documents/Source/fb-rotate/fb-rotate -d 0x1cab4f8c -r 270' param1= terminal=false"
-
-# echo "Mirror (Disable) | bash='/Users/walrusx4/Documents/Source/fb-rotate/fb-rotate -d 0x1cab4f8c -r 0 && /Users/walrusx4/mirror-screen/mirror -l QHD270 1'"
-
-# echo "Disable (Restore) | bash='/Users/walrusx4/mirror-screen/mirror && /Users/walrusx4/Documents/Source/fb-rotate/fb-rotate -d 0x1cab4f8c -r 270'"
-
-# echo "Mirror (Disable) | bash='$parent_path/mirror_swap/mirror_swap_on.sh' terminal=false"
-
-# echo "Disable (Restore) | bash='$parent_path/mirror_swap/mirror_swap_off.sh' terminal=false"
 
 if [[ "${mirror_stat}" == "off" ]]; then
 	echo "Swap Setting | bash='$parent_path/mirror_swap_exec/mirror_swap_on.sh' terminal=false refresh=true"
